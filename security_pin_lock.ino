bd37ab290d2771ddf9a3769b98f22ee6def877ec
GABRIELOFGOD <114562466+GABRIELOFGOD@users.noreply.github.com>
2025-09-16 16:03:57 +0100

    Update security_pin_lock.ino

--- security_pin_lock.ino
+++ security_pin_lock.ino
@@ -9,17 +9,29 @@
 #define RED_LED 7
 #define BUZZER 8
 
+// Try 0x3F if 0x27 doesn't work
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int setPin[3];
 int enteredPin[3];
 int pinIndex = 0;
 bool pinSet = false;
+bool displayUpdated = false;
 
 void setup() {
-  lcd.begin(16, 2);
+  Serial.begin(9600); // For debugging
+  
+  // Initialize LCD
+  lcd.init();
   lcd.backlight();
   lcd.clear();
+  
+  // Test display
+  lcd.setCursor(0, 0);
+  lcd.print("LCD Test");
+  lcd.setCursor(0, 1);
+  lcd.print("Working!");
+  delay(2000);
 
   pinMode(BUTTON1, INPUT_PULLUP);
   pinMode(BUTTON2, INPUT_PULLUP);
@@ -29,90 +41,168 @@ void setup() {
   pinMode(RED_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
 
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("System Ready");
+  Serial.println("System Ready");
   delay(1000);
   lcd.clear();
 }
 
 void loop() {
+  // Check reset button
   if (digitalRead(RESET_BUTTON) == LOW) {
     resetPin();
     delay(500);
+    return;
   }
 
   if (!pinSet) {
-    lcd.setCursor(0, 0);
-    lcd.print("Set PIN:");
+    // Display "Set PIN:" only once
+    if (!displayUpdated) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Set PIN:");
+      displayUpdated = true;
+      Serial.println("Waiting for PIN setup...");
+    }
+    
     getInput(setPin);
-    if (pinIndex == 3) {
+    
+    if (pinIndex >= 3) {
       pinSet = true;
       pinIndex = 0;
+      displayUpdated = false;
       lcd.clear();
       lcd.print("PIN Saved!");
-      delay(1000);
+      Serial.println("PIN Saved!");
+      delay(1500);
       lcd.clear();
     }
   } else {
-    lcd.setCursor(0, 0);
-    lcd.print("Enter PIN:");
+    // Display "Enter PIN:" only once
+    if (!displayUpdated) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Enter PIN:");
+      displayUpdated = true;
+      Serial.println("Waiting for PIN entry...");
+    }
+    
     getInput(enteredPin);
-    if (pinIndex == 3) {
+    
+    if (pinIndex >= 3) {
       checkPin();
       pinIndex = 0;
+      displayUpdated = false;
     }
   }
 }
 
 void getInput(int arr[]) {
+  static unsigned long lastButtonPress = 0;
+  unsigned long currentTime = millis();
+  
+  // Debounce delay
+  if (currentTime - lastButtonPress < 200) {
+    return;
+  }
+
   if (digitalRead(BUTTON1) == LOW) {
-    arr[pinIndex++] = 1;
-    lcd.setCursor(pinIndex - 1, 1);
+    arr[pinIndex] = 1;
+    lcd.setCursor(pinIndex, 1);
     lcd.print("*");
-    delay(300);
-    while (digitalRead(BUTTON1) == LOW);
+    pinIndex++;
+    lastButtonPress = currentTime;
+    Serial.print("Button 1 pressed, index: ");
+    Serial.println(pinIndex);
+    
+    // Wait for button release
+    while (digitalRead(BUTTON1) == LOW) {
+      delay(10);
+    }
   }
-
-  if (digitalRead(BUTTON2) == LOW) {
-    arr[pinIndex++] = 2;
-    lcd.setCursor(pinIndex - 1, 1);
+  
+  else if (digitalRead(BUTTON2) == LOW) {
+    arr[pinIndex] = 2;
+    lcd.setCursor(pinIndex, 1);
     lcd.print("*");
-    delay(300);
-    while (digitalRead(BUTTON2) == LOW);
+    pinIndex++;
+    lastButtonPress = currentTime;
+    Serial.print("Button 2 pressed, index: ");
+    Serial.println(pinIndex);
+    
+    // Wait for button release
+    while (digitalRead(BUTTON2) == LOW) {
+      delay(10);
+    }
   }
-
-  if (digitalRead(BUTTON3) == LOW) {
-    arr[pinIndex++] = 3;
-    lcd.setCursor(pinIndex - 1, 1);
+  
+  else if (digitalRead(BUTTON3) == LOW) {
+    arr[pinIndex] = 3;
+    lcd.setCursor(pinIndex, 1);
     lcd.print("*");
-    delay(300);
-    while (digitalRead(BUTTON3) == LOW);
+    pinIndex++;
+    lastButtonPress = currentTime;
+    Serial.print("Button 3 pressed, index: ");
+    Serial.println(pinIndex);
+    
+    // Wait for button release
+    while (digitalRead(BUTTON3) == LOW) {
+      delay(10);
+    }
   }
 }
 
 void checkPin() {
   lcd.clear();
-  if (setPin[0] == enteredPin[0] && setPin[1] == enteredPin[1] && setPin[2] == enteredPin[2]) {
+  bool correct = true;
+  
+  for (int i = 0; i < 3; i++) {
+    if (setPin[i] != enteredPin[i]) {
+      correct = false;
+      break;
+    }
+  }
+  
+  if (correct) {
     lcd.print("PIN CORRECT!");
     digitalWrite(GREEN_LED, HIGH);
+    Serial.println("PIN CORRECT!");
     delay(2000);
     digitalWrite(GREEN_LED, LOW);
   } else {
     lcd.print("INCORRECT PIN");
     digitalWrite(RED_LED, HIGH);
     digitalWrite(BUZZER, HIGH);
+    Serial.println("INCORRECT PIN");
     delay(2000);
     digitalWrite(RED_LED, LOW);
     digitalWrite(BUZZER, LOW);
   }
+  
   lcd.clear();
+  
+  // Clear entered PIN array
+  for (int i = 0; i < 3; i++) {
+    enteredPin[i] = 0;
+  }
 }
 
 void resetPin() {
   pinSet = false;
   pinIndex = 0;
+  displayUpdated = false;
   lcd.clear();
   lcd.print("PIN Reset");
-  delay(1000);
+  Serial.println("PIN Reset");
+  
+  // Clear both arrays
+  for (int i = 0; i < 3; i++) {
+    setPin[i] = 0;
+    enteredPin[i] = 0;
+  }
+  
+  delay(1500);
   lcd.clear();
 }
